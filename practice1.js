d93085843d8f3a976edd8d7a4d9564696024f393
milu281918 <milu281918@gmail.com>
2023-10-21 16:38:00 +0600

    g

---
+++ practice1.js
@@ -0,0 +1,20 @@
+//concate a string with a number and find the type
+let a= "I am"
+console.log(a+" "+25)
+console.log(typeof (a+" "+25))
+//a constant object
+const obj={
+  employee: "Milu",
+  salary: "0k"
+}
+obj["company"]=["none"]
+//a dictionary of 5 words
+console.log(obj)
+let dict={
+  banana:"yellow",
+  strawberry:"pink",
+  mandarine:"orange",
+  mango:"green",
+  apple:"red"
+}
+console.log(dict["banana"])
