41dc474caed96cc4a27623c22b278fe5f51a84db
milu281918 <milu281918@gmail.com>
2023-10-23 14:18:02 +0600

    f

--- practice2.js
+++ practice2.js
@@ -1,5 +1,6 @@
 //problem1
 let age= prompt("What is your age?");
+age=Number.parseInt(age);
 if(age>=10&&age<=20){
   alert("Valid");
 }
@@ -8,6 +9,7 @@ else{
 }
 //problem2
 let num= prompt("What is your num?");
+num=Number.parseInt(num);
 if(num%3==0 && num%2==0){
   alert("Valid");
 }
@@ -16,6 +18,7 @@ else{
 }
 //problem3
 let num1= prompt("What is your num?");
+num1=Number.parseInt(num1);
 if(num1%3==0 || num1%2==0){
   alert("Valid");
 }
@@ -24,5 +27,6 @@ else{
 }
 //problem5
 let age1=prompt("what is your age?")
+age1=Number.parseInt(age1);
 let a=(age1<18?"not drive":"drive");
 alert("you can "+a);
